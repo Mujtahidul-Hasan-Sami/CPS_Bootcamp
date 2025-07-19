f559b2b3d9c556d8c31cf093f9424b3416837bd3
Mujtahidul-Hasan-Sami <mujtahidulhasansami@gmail.com>
2025-07-20 05:07:20 +0600

    Add files via upload

---
+++ Contest01/c.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double r; cin>>r;
+    double pi = M_PI;
+    double cir = 2*pi*r;
+    double area = pi*r*r;
+    cout<<fixed<<setprecision(6)<<area<< " " <<cir;
+    
+}
---
+++ Contest01/f.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main() {
+    int a; cin>>a;
+    long long ans = a*a*a;
+    cout<<ans<<endl;
+}
